4e3f1848948e2653a46369976605f026a870bb6e
Svetlana1200 <sem.svetlana.m@mail.ru>
2021-11-07 18:27:52 +0500

    Added age and type filter

--- src/main/java/ru/matmex/animalshelter/controller/AnimalsController.java
+++ src/main/java/ru/matmex/animalshelter/controller/AnimalsController.java
@@ -3,8 +3,16 @@ package ru.matmex.animalshelter.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.matmex.animalshelter.model.*;
 import ru.matmex.animalshelter.repository.AnimalRepository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class AnimalsController {
@@ -13,7 +21,35 @@ public class AnimalsController {
 
     @GetMapping("/animals")
     public String animals(Model model) {
-        model.addAttribute("animals", animalRepository.findAll());
+        var animals = animalRepository.findAll();
+        model.addAttribute("animals", animals);
+        model.addAttribute("ageFrom", 1);
+        model.addAttribute("ageTo", animalRepository.findFirstByOrderByAgeDesc().getAge());
+        model.addAttribute("ageMax", animalRepository.findFirstByOrderByAgeDesc().getAge());
+
+        return "animals";
+    }
+
+
+    @GetMapping("/filter")
+    public String animalsFilter(Model model, @ModelAttribute("ageFrom") Integer ageFrom, @ModelAttribute("ageTo") Integer ageTo, @ModelAttribute("type") String type) {
+        var animalsFilteredAge = animalRepository.findByAgeGreaterThanEqualAndAgeLessThanEqual(ageFrom, ageTo);
+        List<Animal> animals = new ArrayList<Animal>();
+
+        if (type.equals("ALL"))
+            animals = animalsFilteredAge;
+        else {
+            var animalsFilteredType = animalRepository.findByType(AnimalType.valueOf(type));
+            for(Animal animal: animalsFilteredType){
+                if (animalsFilteredAge.contains(animal)){
+                    animals.add(animal);
+                }
+            }
+        }
+        model.addAttribute("animals", animals);
+        model.addAttribute("ageFrom", ageFrom);
+        model.addAttribute("ageTo", ageTo);
+        model.addAttribute("ageMax", animalRepository.findFirstByOrderByAgeDesc().getAge());
 
         return "animals";
     }
--- src/main/java/ru/matmex/animalshelter/repository/AnimalRepository.java
+++ src/main/java/ru/matmex/animalshelter/repository/AnimalRepository.java
@@ -3,8 +3,13 @@ package ru.matmex.animalshelter.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.matmex.animalshelter.model.Animal;
+import ru.matmex.animalshelter.model.AnimalType;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
     Animal getOne(long id);
+    List<Animal> findByAgeGreaterThanEqualAndAgeLessThanEqual(Integer ageFrom, Integer ageTo);
+    List<Animal> findByType(AnimalType type);
+    Animal findFirstByOrderByAgeDesc();
 }
